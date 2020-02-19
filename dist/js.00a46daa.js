54fd6fe27a7753584cb24d1720140b8817091256
Sibo Wang <375973811@qq.com>
2020-02-20 12:42:27 +1300

    -

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60558" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54273" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
